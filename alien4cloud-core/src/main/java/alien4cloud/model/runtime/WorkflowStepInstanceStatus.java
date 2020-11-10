953dab3dc3aaa1d9cff4b148594dad59ca4c0a7d
adrian <adi.spataru92@gmail.com>
2020-11-10 22:25:59 +0200

    Revert "Initial commit"
    This reverts commit b09c7ca4c1fc6955fd197264307d8b6676a87e7a.

--- alien4cloud-core/src/main/java/alien4cloud/model/runtime/WorkflowStepInstanceStatus.java
+++
@@ -1,9 +0,0 @@
-package alien4cloud.model.runtime;
-
-/**
- * Status of a workflow step instance.
- */
-public enum WorkflowStepInstanceStatus {
-    STARTED,
-    COMPLETED
-}
--- alien4cloud-dao/src/main/java/alien4cloud/dao/IESMetaPropertiesSearchContextBuilder.java
+++
@@ -1,6 +0,0 @@
-package alien4cloud.dao;
-
-public interface IESMetaPropertiesSearchContextBuilder {
-
-    <T> IESMetaPropertiesSearchContext getContext(Class<T> clazz);
-}
